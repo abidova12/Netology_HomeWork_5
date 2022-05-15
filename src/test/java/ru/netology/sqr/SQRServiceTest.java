418ae42e029234b649c765dfeaed7e0792154513
Zhanara <abidova12@gmail.com>
2022-05-16 00:13:18 +0300

    fixed #3

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,14 +1,33 @@
 package ru.netology.sqr;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SQRServiceTest {
 
     @Test
-    public void test() {
+    public void testAllRight() {
         SQRService service = new SQRService();
-        int actual = service.numerical(1_000, 10_000);
-        int expected = 68;
+        int actual = service.numerical(100, 196);
+        int expected = 5;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMinus() {
+        SQRService service = new SQRService();
+        int actual = service.numerical(101, 195);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testPlus() {
+        SQRService service = new SQRService();
+        int actual = service.numerical(99, 197);
+        int expected = 5;
 
         Assertions.assertEquals(expected, actual);
     }
